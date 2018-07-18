8cf718783d94ab391a68f67f7775ca71053fc48f
kyle <kyle.script@gmail.com>
2018-07-18 10:52:33 +0800

    init code

---
+++ anyone.js
@@ -0,0 +1,90 @@
+/*
+ 从百度搜索排行榜上爬取一些名人，作为用户名字
+*/
+
+const http = require('http');
+const iconv = require('iconv-lite');
+const BufferHelper = require('bufferhelper');
+
+let user_names = [];
+
+function get_hot_man(callback) {
+    http.get('http://top.baidu.com/category?c=9', (res) => {
+        let data = new BufferHelper();
+        res.on('data', d => {
+            data.concat(d);
+        });
+        res.on('end', () => {
+            data = iconv.decode(data.toBuffer(), 'gb2312');
+            let result = [];
+
+            let last_pos = 0;
+            let start = "<a target=\"_blank\" title=\"";
+            let pos = data.indexOf(start, last_pos);
+            while (pos !== -1) {
+                last_pos = pos + start.length + 1;
+                let pos_point = data.indexOf("\"", last_pos);
+                if (pos_point > 0) {
+                    last_pos = pos_point + 1;
+                    result.push(data.substring(pos + start.length, pos_point));
+                }
+                pos = data.indexOf(start, last_pos);
+            }
+            callback(result);
+        });
+    }).on('error', (e) => {
+        console.error(e);
+    });
+}
+
+// 初始化名字库
+exports.init = () => {
+    get_hot_man(famous => {
+        for (let name in famous) {
+            user_names.push({"name": famous[name], "used": false});
+        }
+    });
+};
+
+// 从名字库中寻找一个可用名字
+exports.find_empty_name = () => {
+    let len = user_names.length;
+
+    // 随机10次找一个没有被占用的名字
+    for (let i = 0; i < 10; i++) {
+        let rand_index = parseInt(Math.random() * len);
+        if (user_names[rand_index]['used'] === false) {
+            user_names[rand_index]['used'] = true;
+            return user_names[rand_index]['name'];
+        }
+    }
+
+    // 从头找第一个没被占用的名字
+    for (let i = 0; i < len; i++) {
+        if (user_names[i]['used'] === false) {
+            user_names[i]['used'] = true;
+            return user_names[i]['name'];
+        }
+    }
+
+    // 名字不够分配了
+    return ""
+};
+
+// 释放名字占用
+exports.set_name_unused = name => {
+    for (let i = 0; i < user_names.length; i++) {
+        if (user_names[i]['name'] === name) {
+            user_names[i]['used'] = false;
+            return;
+        }
+    }
+};
+
+exports.is_name_used = name => {
+    for (let i = 0; i < user_names.length; i++) {
+        if (user_names[i]['name'] === name) {
+            return user_names[i]['used'];
+        }
+    }
+};
---
+++ server.js
@@ -0,0 +1,92 @@
+// 定义消息前缀，表示特定的含义
+const prefix_new_id = '[ID_NEW]';       // 分配名字给新用户
+const prefix_change_id = '[ID_CHANGE]'; // 老用户换名字
+const prefix_use_id = '[ID_USE]';       // 老用户用cookie里的老名字
+const prefix_message_text = '[MESSAGE_TEXT]';   // 文本消息
+const prefix_message_photo = '[MESSAGE_PHOTO]'; // 图片消息
+// 文本消息名字、内容分隔符
+const split_str = "#9527#";
+
+const anyone = require('./anyone.js');
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({port: 9527});
+
+// 给WebSocket类添加一个user属性，以区分用户
+WebSocket.prototype.user = "";
+
+// 初始化备用用户名
+anyone.init();
+
+// 广播用户消息, toUser=-1 表示全员广播
+function send_user_message(toUser, owner, message) {
+    // 广播
+    if (toUser === -1) {
+        wss.clients.forEach(function (client) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(owner + split_str + message);
+            }
+        });
+        return;
+    }
+
+    // 发给特定用户
+    wss.clients.forEach(function (client) {
+        if (client.user === toUser && client.readyState === WebSocket.OPEN) {
+            client.send(owner + split_str + message);
+            return;
+        }
+    });
+}
+
+// 新的用户来了
+wss.on('connection', ws => {
+    console.log("New User connected...");
+
+    // 处理客户端发来的消息
+    ws.on('message', m => {
+        console.log('Recv:' + m);
+        if (m.startsWith(prefix_new_id)) {  // 分配名字给新用户
+            // 已经有名字了，客户端逻辑出问题了，不做任何处理
+            if (ws.user !== "") {
+                return;
+            }
+            ws.user = anyone.find_empty_name();
+            ws.send(prefix_new_id + ws.user);
+        } else if (m.startsWith(prefix_change_id)) { // 老用户换名字
+            // 还没有名字，客户端逻辑出问题了，不做任何处理
+            if (ws.user === "") {
+                return;
+            }
+            anyone.set_name_unused(ws.user);
+            ws.user = anyone.find_empty_name();
+            ws.send(prefix_new_id + ws.user);
+        } else if (m.startsWith(prefix_use_id)) { // 老用户用cookie里的老名字
+            // 已经有名字了，客户端逻辑出问题了，不做任何处理
+            if (ws.user !== "") {
+                return;
+            }
+            // 已经被占用了, 分配一个新名字吧
+            if (anyone.is_name_used(m.substring(prefix_use_id.length))) {
+                ws.user = anyone.find_empty_name();
+            } else {
+                ws.user = m.substring(prefix_use_id.length);
+            }
+            ws.send(prefix_new_id + ws.user);
+        } else if (m.startsWith(prefix_message_text)) { // 文本消息
+            // 广播
+            send_user_message(-1, ws.user, m.substring(prefix_message_text.length));
+        } else if (m.startsWith(prefix_message_photo)) { // 图片消息
+            console.log('not support photo message.');
+        }
+    });
+
+    // 客户端断开连接
+    ws.on('close', (client) => {
+        console.log("disconnected:" + ws.user);
+        anyone.set_name_unused(ws.user);
+    });
+});
+
+wss.on('error', err => {
+    console.log('ERROR:' + err);
+});
